add72055426a7f6119c5be9311942ad085b58adf
Jessica Hanke <122949564+jnhanke1@users.noreply.github.com>
2023-04-28 14:21:39 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using lab_11_jnhanke1.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ProfessorContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("ProfessorContext")));
+
+var app = builder.Build();
+
+//call my seed data: 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider; 
+
+    SeedData.Initialize(services); 
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
